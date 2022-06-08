d2ba45a5cb0f8e481080ad59508a60324b0f7c19
Daria <emelanenkodara@gmail.com>
2022-06-08 21:58:10 +0300

    second task

---
+++ Program2.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+List<int> anArray = new List<int>();
+anArray.Add(num1);
+anArray.Add(num2);
+anArray.Add(num3);
+
+int m = anArray.Max();
+ Console.WriteLine("max = {0}", m );
+ 
+           
