4bbc7942dbd79b9225820cf7630c6fd0bf5a1f38
Daria <emelanenkodara@gmail.com>
2022-06-08 22:16:53 +0300

    fourh task

---
+++ Program4.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: N ");
+int N = Convert.ToInt32(Console.ReadLine());
+List<int> anArray = new List<int>();
+for (int i = 1; i <= N; i++)
+{
+    if ((i % 2) == 0)
+    {
+        anArray.Add(i);
+    }
+}        
+Console.WriteLine("{0}", string.Join(", ", anArray));
