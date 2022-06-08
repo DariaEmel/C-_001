851d7d440dea187011c96b198b6212bc16779db1
Daria <emelanenkodara@gmail.com>
2022-06-08 22:06:53 +0300

    third task

---
+++ Program3.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if ((num1 % 2) == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+ 
+           
